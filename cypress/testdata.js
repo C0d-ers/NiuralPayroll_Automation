9ce8bc5689d27ce1b5e5eeeb411b954962100658
ChaudharyDBijay <bijay.chaudhary@8squarei.com>
2024-06-09 01:02:25 +0545

    Registration data

---
+++ cypress/testdata.js
@@ -0,0 +1,11 @@
+export const registerUser = {
+  firstName: "John",
+  middleName: "William",
+  lastName: "Smith",
+  email: "john.smit0.05h@uhcir094.mailosaur.net",
+  companyName: "ABC Consulting",
+  companyWebsite: "www.john.abcconsulting.com",
+  businessClientsOption: "5-10 clients",
+  password: "Password@123",
+  phoneNumber: "2345678901",
+};
