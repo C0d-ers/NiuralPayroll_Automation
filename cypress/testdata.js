2313a9b0839a96df5397fbe157872fff7797edd5
ChaudharyDBijay <bijay.chaudhary@8squarei.com>
2024-06-12 08:44:19 +0545

    changed the test date, added comment for guidance

--- cypress/testdata.js
+++ cypress/testdata.js
@@ -1,68 +1,79 @@
-export const adminUser = {
+//NOTE: Do not change the email domain, it must be "@uhcir094.mailosaur.net"
+
+export const loginUser = {
   firstName: "Michael",
   email: "michael.brown@uhcir094.mailosaur.net",
   password: "Password@123",
   companyName: "NextGen Innovations",
 };
-
-export const registeredClient = {
-  companyName: "TechSavvy Solutions",
-  firstName: "Emily",
-  lastName: "Johnson",
-  email: "emily.johnson@uhcir094.mailosaur.net",
-};
+//This is to test login flow, so a registered user is required.
+//No need to change the details for this.
 
 export const registerUser = {
-  firstName: "Michael",
+  firstName: "Oliver",
   middleName: "James",
-  lastName: "Brown",
-  email: "michael.brown@uhcir094.mailosaur.net",
-  companyName: "NextGen Innovations",
-  companyWebsite: "www.michael.nextgeninnovations.com",
+  lastName: "Smith",
+  email: "oliver.smith789@uhcir094.mailosaur.net",
+  companyName: "Creative Solutions LLC",
+  companyWebsite: "www.creativesolutions.com",
   businessClientsOption: "5-10 clients",
   password: "Password@123",
-  phoneNumber: "1122334455",
+  phoneNumber: "1234567890",
 };
+//This are the details that are need to onboard niural payroll partner
+//Since it's for registration, we need to atleast change the email everytime running the test script "cypress/e2e/1signup.cy.js"
 
-export const oldclientRegistration = {
-  companyName: "AlphaTech Solutions",
-  dbaName: "AlphaTech",
-  numOfCurrentEmployee: "40",
-  numOfCurrentContractors: "15",
-  firstName: "Jessica",
-  middleName: "M",
-  lastName: "Davis",
-  email: "jessica.davis@uhcir094.mailosaur.net",
-  companyWebsite: "www.alphatech.com",
-  employeeNumber: "Between 25 and 50",
-  phoneExtension: "+1",
-  phoneNumber: "5566778899",
+export const adminUser = {
+  firstName: "Sophia",
+  email: "sophia.johnson456@uhcir094.mailosaur.net",
+  password: "Password@123",
+  companyName: "Innovative Technologies Inc",
 };
+//These are the details form the onboarded niural payroll partner, extracted from registerUser
+//Need to update accordingly with respect to "registerUser".
+//Can still proceed without changing this detail
 
 export const clientRegistration = {
-  companyName: "TechSavvy Solutions",
-  dbaName: "TechSavvy",
-  numOfCurrentEmployee: "25",
-  numOfCurrentContractors: "5",
-  firstName: "Emily",
-  middleName: "J",
-  lastName: "Johnson",
-  email: "emily.johnson@uhcir094.mailosaur.net",
-  companyWebsite: "www.techsavvy.com",
+  companyName: "Innovative Tech Solutions",
+  dbaName: "Innovative Tech",
+  numOfCurrentEmployee: "55",
+  numOfCurrentContractors: "8",
+  firstName: "Emma",
+  middleName: "Grace",
+  lastName: "Taylor",
+  email: "emma.taylor456@uhcir094.mailosaur.net",
+  companyWebsite: "www.innovativetechsolutions.com",
   employeeNumber: "Between 25 and 50",
   phoneExtension: "+1",
-  phoneNumber: "5559876543",
+  phoneNumber: "1222560090",
 };
+
+//These are the details that are used for client registration after a niural partner logs in.
+//Need to Change as we cannot onboard same client again and again.
+//need to change when running "cypress/e2e/3clientManagement.cy.js" everytime
+
+export const registeredClient = {
+  companyName: "Innovative Tech Solutions",
+  firstName: "Emma",
+  lastName: "Taylor",
+  email: "emma.taylor456@uhcir094.mailosaur.net",
+};
+//These are the details of added client, Asserting these details will allow to further onboard the client details
+//Change details in accordance with "clientRegistration"
+//Above data should be same as "clientRegistration" regardless of if any script is executed or not.
 
 export const registerCompany = {
   country: "United States",
-  EIN: "369251478",
-  registeredDate: "12-01-2023",
-  businessEntityType: "Limited liability company",
+  EIN: "123456789",
+  registeredDate: "05-15-2024",
+  businessEntityType: "LLC",
   natureOfBusiness: "Not Known",
-  addressLine1: "New Address Line 1",
-  addressLine2: "New Address Line 2",
-  city: "New City",
-  state: "New State",
-  zipCode: "12345",
+  addressLine1: "789 Maple Avenue",
+  addressLine2: "Suite 300",
+  city: "New York",
+  state: "New York",
+  zipCode: "10001",
 };
+//These are the details to register company of the client.
+//This needs to be change as we cannot register same company again and again
+//need to change when running "cypress/e2e/4clientCompanyOnboarding.cy.js" everytime
