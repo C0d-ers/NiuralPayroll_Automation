d7a3d7e87022b627c774062922e31502c4ded880
ChaudharyDBijay <bijay.chaudhary@8squarei.com>
2024-06-16 16:31:55 +0545

    Added data for one step verification

--- cypress/fixtures/ClientDetails_TestData.js
+++ cypress/fixtures/ClientDetails_TestData.js
@@ -1,17 +1,17 @@
 //NOTE: Do not change the email domain, it must be "@uhcir094.mailosaur.net"
 export const clientRegistration = {
-  companyName: "Innovative Tech Solutions",
-  dbaName: "Innovative Tech",
-  numOfCurrentEmployee: "55",
-  numOfCurrentContractors: "8",
-  firstName: "Emma",
-  middleName: "Grace",
-  lastName: "Taylor",
-  email: "emma.taylor456@uhcir094.mailosaur.net",
-  companyWebsite: "www.innovativetechsolutions.com",
+  companyName: "Konoha Enterprises",
+  dbaName: "Konoha",
+  numOfCurrentEmployee: "20",
+  numOfCurrentContractors: "5",
+  firstName: "Naruto",
+  middleName: "Sage",
+  lastName: "Uzumaki",
+  email: "naruto.uzumaki@uhcir094.mailosaur.net",
+  companyWebsite: "www.konohaenterprises.com",
   employeeNumber: "Between 25 and 50",
   phoneExtension: "+1",
-  phoneNumber: "1222560090",
+  phoneNumber: "1333567890",
 };
 
 //These are the details that are used for client registration after a niural partner logs in.
@@ -19,10 +19,10 @@ export const clientRegistration = {
 //need to change when running "cypress/e2e/3clientManagement.cy.js" everytime
 
 export const registeredClient = {
-  companyName: "Innovative Tech Solutions",
-  firstName: "Emma",
-  lastName: "Taylor",
-  email: "emma.taylor456@uhcir094.mailosaur.net",
+  companyName: clientRegistration.companyName,
+  firstName: clientRegistration.firstName,
+  lastName: clientRegistration.lastName,
+  email: clientRegistration.email,
 };
 //These are the details of added client, Asserting these details will allow to further onboard the client details
 //Change details in accordance with "clientRegistration"
